dc7d7f055d3f26d1cd2ca6b52e1213c64d45b182
ajoseph15-siths <ajoseph15@schools.nyc.gov>
2026-01-01 02:36:13 -0500

    Add files via upload

---
+++ Day11a.py
@@ -0,0 +1,48 @@
+# FUNCTIONS
+# 1. Create a list to hold the contents of the input file
+def get_file_data(file_name):
+    f = open(file_name)
+    data = []
+    for line in f:
+        data.append(line.rstrip())
+    return data
+# 2. Create devices from each line in the input file
+def create_devices(input_list):
+   all_devices = []
+   for line in input_list:
+       i = line.index(':')
+       # separate the line into device id: connected devices
+       device_id = line[:i]
+       connected_devices = tuple(line[i+1:].split())
+       all_devices.append((device_id, connected_devices))
+   return all_devices
+# 3. Find a way out, given a "starting" device
+def find_way_out(device_id, all_devices, count):
+    # find connected devices
+    connected_devices = []
+    for device in all_devices:
+        if device[0] == device_id:
+            connected_devices = device[1]
+    # loop through the connected devices
+    for connected_device in connected_devices:
+        if connected_device == "out":
+            print("found a way out")
+            count += 1
+        else:
+            find_way_out(connected_device, all_devices, count)
+    return count
+
+def count_ways_out():
+    count = 0
+    count += find_way_out("you",all_devices,0)
+    print("Final count =",count)
+
+# COMMANDS
+# 1. get info from input file
+input_list = get_file_data('input.txt')
+# 2. create devices from input file and add to a list
+all_devices = create_devices(input_list)
+# 3. find the ways out
+# this doesn't actually work... i had to run it and copy and paste the output
+# into notepad++ and then count how many times it printed "found a way out"
+print(find_way_out("you", all_devices,0))
